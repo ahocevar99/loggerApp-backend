ef92d69de8bda55d394dd2aaf4a772ae5f0e4518
ahocevar99 <116942467+ahocevar99@users.noreply.github.com>
2025-05-26 23:25:06 +0200

    fixed bug with app.use ("*")

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ const startServer = async () => {
 
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
-  app.use("/", router);
+  app.use(/(.*)/, router);
 
   app.use((err, req, res, next) => {
     if (err.name === "UnauthorizedError") {
